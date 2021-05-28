fc08eba3103383766322cfebf1bab9d7832ef20d
Nathan Richards <nathan.k.richards@gmail.com>
2021-05-28 16:07:19 -0400

    macropad firmware

---
+++ keyboards/keebio/bdn9/keymaps/jeff/keymap.c
@@ -0,0 +1,82 @@
+/* Copyright 2019 Danny Nguyen <danny@keeb.io>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum encoder_names {
+  _LEFT,
+  _RIGHT,
+  _MIDDLE,
+};
+
+
+enum custom_keycodes {
+    EXCEL = 0x00,
+    LG_BRIGHT_UP = 0x01,
+    LG_BRIGHT_DOWN = 0x02
+};
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case EXCEL:
+            if (record->event.pressed) {
+                SEND_STRING(SS_TAP(X_LGUI) SS_DELAY(100) "excel" SS_DELAY(150) SS_TAP(X_ENT));
+            } else {
+                // when keycode is released
+            }
+            break;
+    }
+    return true;
+}
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+        | Knob 1: Vol Dn/Up | Knob 2: Screen Brightness | Knob 2: Page Dn/Up |
+        | Press: Mute       | --                        | --                 |
+        | Press: Excel      | Up                        | RGB Mode           |
+        | Left              | Down                      | Right              |
+     */
+    [0] = LAYOUT(
+        KC_MUTE, KC_NO, KC_NO,
+        EXCEL  , KC_UP  , RGB_MOD,
+        KC_LEFT, KC_DOWN, KC_RGHT
+    )
+};
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == _LEFT) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    }
+    else if (index == _MIDDLE) {
+        if (clockwise) {
+            tap_code(KC_F24);
+        } else {
+            tap_code(KC_F23);
+        }
+    }
+    else if (index == _RIGHT) {
+        if (clockwise) {
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
+        }
+    }
+}
